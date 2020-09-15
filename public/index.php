0266b6a928dcbe4beadf1b032f13bfa3c402bfdf
Kelly Mersereau <kelly.mersereau@gmail.com>
2020-09-14 22:23:59 -0400

    fixing stuff

--- public/index.php
+++ public/index.php
@@ -58,22 +58,3 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
-    
-    
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("test@example.com", "Example User");
-    $email->setSubject("Sending with SendGrid is Fun");
-    $email->addTo("test@example.com", "Example User");
-    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-    $email->addContent(
-        "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-    );
-    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-    try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
-    } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
-    }
